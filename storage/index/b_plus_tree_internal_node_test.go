c3052e92b1a6ac9ac9d26ece5df8339a0cdf3a9a
ue-sho <tssu45@gmail.com>
2023-01-07 23:20:42 +0900

    test: InternalNodeのテスト修正

--- storage/index/b_plus_tree_internal_node_test.go
+++ storage/index/b_plus_tree_internal_node_test.go
@@ -1,9 +1,7 @@
 package index
 
 import (
-	"bytes"
 	"encoding/binary"
-	"fmt"
 	"testing"
 
 	"github.com/ue-sho/ohako/storage/page"
@@ -43,104 +41,83 @@ func TestInternalNodeInsert(t *testing.T) {
 	}{
 		// 挿入されたkey [ 5, 8, 11 ]
 		{1, 1},  // key=5より小さいので、5のPageIDを参照
-		{5, 3},  // 5と一致している、次のPageIDを参照
-		{6, 3},  // 5と一致している、次のPageIDを参照
-		{8, 4},  // 5と一致している、次のPageIDを参照
-		{10, 4}, // 5と一致している、次のPageIDを参照
-		{11, 2}, // 5と一致している、次のPageIDを参照
-		{12, 2}, // 5と一致している、次のPageIDを参照
+		{5, 3},  // key=5と一致しているので、5の次のPageIDを参照
+		{6, 3},  // key=5より大きく、key=8より小さいので、8のPageIDを参照
+		{8, 4},  // key=8と一致しているので、8の次のPageIDを参照
+		{10, 4}, // key=8より大きく、key=11より小さいので、11のPageIDを参照
+		{11, 2}, // key=11と一致しているので、11の次のPageIDを参照 → RightNodeを参照する
+		{12, 2}, // 挿入された値の中で最大値のkey=11より大きいので、RightNodeを参照する
 	}
 	for _, tt := range tests {
 		actual := internalNode.SearchChild(uint64ToBytes(tt.key))
-		fmt.Println("internalNode.SearchChild ", tt.key, tt.pageID)
-
 		testingpkg.Equals(t, tt.pageID, actual)
 	}
 
 }
 
-func TestInterNewInternalNode(t *testing.T) {
-	uint64ToBytes := func(n uint64) []byte {
-		buf := make([]byte, 8)
-		binary.BigEndian.PutUint64(buf, n)
-		return buf[:]
+func TestInternalNodeSplit(t *testing.T) {
+	// given
+	data := make([]byte, 100)
+	internalNode := NewInternalNode(data)
+
+	// { key=5, value=PageID(1) } を挿入し、rightNodeはPageID(2)を参照させる
+	internalNode.Initialize(uint64ToBytes(5), page.PageID(1), page.PageID(2))
+
+	// { key=8, value=PageID(3) } を挿入する
+	err := internalNode.Insert(1, uint64ToBytes(8), page.PageID(3))
+	if err != nil {
+		panic(err)
+	}
+	// { key=11, value=PageID(4) } を挿入する
+	err = internalNode.Insert(2, uint64ToBytes(11), page.PageID(4))
+	if err != nil {
+		panic(err)
 	}
 
-	t.Run("Insert", func(t *testing.T) {
+	// 2つ目のInternalNodeを作成する
+	data2 := make([]byte, 100)
+	internalNode2 := NewInternalNode(data2)
 
-	})
+	// when: { key=10, value=PageID(5) }を起点に分割挿入する
+	midKey := internalNode.SplitInsert(internalNode2, uint64ToBytes(10), page.PageID(5))
 
-	t.Run("Split", func(t *testing.T) {
-		var err error
+	// then: 分割の分岐点はkey=8 (8がRootとなり分割される)
+	// 初期のInternalNodeのペア数は2, 新規InternalNodeのペア数は1に分割される
+	testingpkg.Equals(t, midKey, uint64ToBytes(8))
+	testingpkg.Equals(t, 2, internalNode.NumPairs())
+	testingpkg.Equals(t, 1, internalNode2.NumPairs())
 
-		data := make([]byte, 100)
-		internalNode := NewInternalNode(data)
+	tests := []struct {
+		key    uint64
+		pageID page.PageID
+	}{
+		// 挿入されたkey [ 10, 11 ]
+		{9, 5},
+		{10, 4},
+		{11, 2},
+		{12, 2},
+	}
+	for _, tt := range tests {
+		actual := internalNode.SearchChild(uint64ToBytes(tt.key))
+		if actual != tt.pageID {
+			t.Fatalf("internalNode.SearchChild(%v) = %v, want %v", tt.key, actual, tt.pageID)
+		}
+	}
 
-		internalNode.Initialize(uint64ToBytes(5), page.PageID(1), page.PageID(2))
-		err = internalNode.Insert(1, uint64ToBytes(8), page.PageID(3))
-		if err != nil {
-			panic(err)
+	tests = []struct {
+		key    uint64
+		pageID page.PageID
+	}{
+		// 挿入されたkey [ 5 ]
+		{1, 1},
+		{5, 3},
+		{6, 3},
+	}
+	for _, tt := range tests {
+		actual := internalNode2.SearchChild(uint64ToBytes(tt.key))
+		if actual != tt.pageID {
+			t.Fatalf("internalNode2.SearchChild(%v) = %v, want %v", tt.key, actual, tt.pageID)
 		}
-		err = internalNode.Insert(2, uint64ToBytes(11), page.PageID(4))
-		if err != nil {
-			panic(err)
-		}
+	}
 
-		data2 := make([]byte, 100)
-		internalNode2 := NewInternalNode(data2)
-		{
-			midKey := internalNode.SplitInsert(internalNode2, uint64ToBytes(10), page.PageID(5))
-			expect := uint64ToBytes(8)
-			if !bytes.Equal(midKey, expect) {
-				t.Fatalf("internalNode.SplitInsert() = %v, want %v", midKey, expect)
-			}
-		}
-		{
-			actual := internalNode.NumPairs()
-			expect := 2
-			if actual != expect {
-				t.Fatalf("internalNode.NumPairs() = %v, want %v", actual, expect)
-			}
-		}
-		{
-			actual := internalNode2.NumPairs()
-			expect := 1
-			if actual != expect {
-				t.Fatalf("internalNode2.NumPairs() = %v, want %v", actual, expect)
-			}
-		}
-		{
-			tests := []struct {
-				key          uint64
-				pageIDPageID page.PageID
-			}{
-				{1, 1},
-				{5, 3},
-				{6, 3},
-			}
-			for _, tt := range tests {
-				actual := internalNode2.SearchChild(uint64ToBytes(tt.key))
-				if actual != tt.pageIDPageID {
-					t.Fatalf("internalNode2.SearchChild(%v) = %v, want %v", tt.key, actual, tt.pageIDPageID)
-				}
-			}
-		}
-		{
-			tests := []struct {
-				key          uint64
-				pageIDPageID page.PageID
-			}{
-				{9, 5},
-				{10, 4},
-				{11, 2},
-				{12, 2},
-			}
-			for _, tt := range tests {
-				actual := internalNode.SearchChild(uint64ToBytes(tt.key))
-				if actual != tt.pageIDPageID {
-					t.Fatalf("internalNode.SearchChild(%v) = %v, want %v", tt.key, actual, tt.pageIDPageID)
-				}
-			}
-		}
-	})
 }
